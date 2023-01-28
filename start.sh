c879b4e284aa4d22696d213926b0870e766c0eb0
lloydchang <lloydchang@gmail.com>
2023-01-28 09:52:17 -0800

    fix: k

--- start.sh
+++ start.sh
@@ -1,8 +1,10 @@
 #!/bin/sh -x
 
-set -x && mkdir -p ~/project/src/app && cp billchurch-webssh2/app/config.json.sample ~/project/src/app/config.json && cp billchurch-webssh2/app/config.json.sample ~/project/src/billchurch-webssh2/app/config.json && cd billchurch-webssh2/app && echo $(npm start | tee ~/project/src/app/log) &
+set -x && mkdir -p ~/project/src/app && cp billchurch-webssh2/app/config.json.sample ~/project/src/app/config.json && cp billchurch-webssh2/app/config.json.sample ~/project/src/billchurch-webssh2/app/config.json && cd billchurch-webssh2/app && echo $(npm start | tee ~/log) &
 
-cd ./ssh2-git && mkdir -p ~/.ssh && echo | ssh-keygen -f ~/.ssh/foo && cp ~/.ssh/foo ./host.key && npm i ssh2 && npm install && cp ~/.ssh/foo.pub . && echo $(node ssh2-server.js | tee ~/project/src/app/log) &
+cd ./ssh2-git && mkdir -p ~/.ssh && echo | ssh-keygen -f ~/.ssh/foo && cp ~/.ssh/foo ./host.key && npm i ssh2 && npm install && cp ~/.ssh/foo.pub . && echo $(node ssh2-server.js | tee ~/log) &
 
-tail -f ~/project/src/app/log
+ls -la ~
+
+tail -f ~/log
 
