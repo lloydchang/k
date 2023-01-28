41532a6cbe55b5c1d24012c9280c378d18d6d0a7
lloydchang <lloydchang@gmail.com>
2023-01-28 12:05:12 -0800

    fix: k

--- start.sh
+++ start.sh
@@ -2,9 +2,7 @@
 
 echo $(set -x && mkdir -p ~/project/src/app && cp billchurch-webssh2/app/config.json.sample ~/project/src/app/config.json && cp billchurch-webssh2/app/config.json.sample ~/project/src/billchurch-webssh2/app/config.json && cd billchurch-webssh2/app && npm start) | tee ~/log &
 
-echo $(cd ./ssh2-git && mkdir -p ~/.ssh && echo | ssh-keygen -f ~/.ssh/foo && cp ~/.ssh/foo ./host.key && npm i ssh2 && npm install && cp ~/.ssh/foo.pub . && node ssh2-server.js) | tee ~/log &
+tail -f ~/log &
 
-ls -la ~
-
-tail -f ~/log
+cd ./ssh2-git && mkdir -p ~/.ssh && echo | ssh-keygen -f ~/.ssh/foo && cp ~/.ssh/foo ./host.key && npm i ssh2 && npm install && cp ~/.ssh/foo.pub . && node ssh2-server.js
 
