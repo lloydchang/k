42f977dbe241628fc07796f8f34b288659562d5a
lloydchang <lloydchang@gmail.com>
2023-01-27 18:36:05 -0800

    fix: k

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -x
 
-set -x && mkdir -p /opt/render/project/src/app && cp billchurch-webssh2/app/config.json.sample /opt/render/project/src/app/config.json && cp billchurch-webssh2/app/config.json.sample /opt/render/project/src/billchurch-webssh2/app/config.json && cd billchurch-webssh2/app && npm start &
+set -x && mkdir -p ~/project/src/app && cp billchurch-webssh2/app/config.json.sample ~/project/src/app/config.json && cp billchurch-webssh2/app/config.json.sample ~/project/src/billchurch-webssh2/app/config.json && cd billchurch-webssh2/app && npm start &
 
 cd .. && cd ./ssh2-git && mkdir -p ~/.ssh && echo | ssh-keygen -f ~/.ssh/foo && cp ~/.ssh/foo ./host.key && npm i ssh2 && npm install && cp ~/.ssh/foo.pub . && node ssh2-server.js
 
